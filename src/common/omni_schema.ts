cfe03f24877671fcb5213591e31811ebdbbe1ff5
Henry Blanchette <henry@monkeyface.com>
2025-03-13 12:45:00 -0400

    checkpoint after removing Domain

--- src/common/omni_schema.ts
+++ src/common/omni_schema.ts
@@ -1,85 +1,34 @@
-// -----------------------------------------------------------------------------
-
-export type Domain =
-  | StringDomain
-  | NumberDomain
-  | BooleanDomain
-  | ArrayDomain<any>
-  | ObjectDomain<{ [key: string]: any }>;
-
-export type StringDomain = { type: "string" };
-export type NumberDomain = { type: "number" };
-export type BooleanDomain = { type: "boolean" };
-export type ArrayDomain<D_items extends Domain> = {
-  type: "array";
-  items: D_items;
-};
-export type ObjectDomain<D_props extends { [key: string]: Domain }> = {
-  type: "object";
-  props: D_props;
-};
-
-export type FromDomain<D extends Domain> = D extends StringDomain
-  ? string
-  : D extends NumberDomain
-    ? number
-    : D extends BooleanDomain
-      ? boolean
-      : D extends ArrayDomain<infer D_items>
-        ? FromDomain<D_items>[]
-        : D extends ObjectDomain<infer D_props>
-          ? {
-              [K in keyof D_props]: D_props[K] extends Domain
-                ? FromDomain<D_props[K]>
-                : never;
-            }
-          : never;
-
-type D_ex1 = ObjectDomain<{
-  a: StringDomain;
-  b: ArrayDomain<BooleanDomain>;
-  c: NumberDomain;
-  d: BooleanDomain;
-}>;
-
-type T_ex1 = FromDomain<D_ex1>;
-
-// -----------------------------------------------------------------------------
-
-type Schema<D extends Domain> =
+type Schema =
   | { type: "string"; description?: string }
   | { type: "number"; description?: string }
   | { type: "boolean"; description?: string }
-  | { type: "array"; items: Schema<any>; description?: string }
+  | { type: "array"; items: Schema; description?: string }
   | {
       type: "object";
-      props: { [key: string]: [number, Schema<any>] };
+      props: { [key: string]: [number, Schema] };
       description?: string;
     };
 
-export function string(description?: string): Schema<StringDomain> {
+export function string(description?: string): Schema {
   return { type: "string", description };
 }
 
-export function number(description?: string): Schema<NumberDomain> {
+export function number(description?: string): Schema {
   return { type: "number", description };
 }
 
-export function boolean(description?: string): Schema<BooleanDomain> {
+export function boolean(description?: string): Schema {
   return { type: "boolean", description };
 }
 
-export function array<D_items extends Domain>(
-  items: Schema<D_items>,
-  description?: string,
-): Schema<ArrayDomain<D_items>> {
+export function array(items: Schema, description?: string): Schema {
   return { type: "array", items, description };
 }
 
-export function object<D_props extends { [key: string]: Domain }>(
-  props: { [K in keyof D_props]: [number, Schema<D_props[K]>] },
+export function object(
+  props: { [key: string]: [number, Schema] },
   description?: string,
-): Schema<ObjectDomain<D_props>> {
+): Schema {
   return { type: "object", props, description };
 }
 
@@ -90,6 +39,6 @@ const schema_ex1 = object({
   d: [3, boolean()],
 });
 
-type GetDomain<S extends Schema<any>> = S extends Schema<infer D> ? D : never;
+// type GetDomain<S extends Schema<any>> = S extends Schema<infer D> ? D : never;
 
-type T_ex1_prime = GetDomain<typeof schema_ex1>;
+// type T_ex1_prime = GetDomain<typeof schema_ex1>;
